979bdbba19c76294d97f87aa88adc39390876689
amwebdevy <amwebdevy@gmail.com>
2019-06-27 18:29:35 -0400

    edited index.php

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
             mandaremus dolor labore nam lorem, quo sunt graviterque. Incididunt aut sunt 
             laborum, ne duis admodum domesticarum, veniam voluptatibus tempor irure quamquam 
             de quamquam enim nulla aut summis.</p>
-          </div>
+          </div><!-- .bio-content-container -->
         </div><!-- .cid-bio -->
         
         <div class="cid-bio">
